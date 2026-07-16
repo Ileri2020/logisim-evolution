88de4811fe43a79e70ff3dc9aabc55603a33939f
Adepoju Ololade Joshua Ilerioluwa <adepojuololade2020@gmail.com>
2026-07-16 03:47:18 +0100

    compile error fix

--- src/main/java/com/cburch/logisim/scripting/PythonScriptManager.java
+++ src/main/java/com/cburch/logisim/scripting/PythonScriptManager.java
@@ -17,6 +17,7 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.Source;
